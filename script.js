f8eb41098e7511989906453d6ae8c36a4b2e2a39
bruno1604 <brunont1211@gmail.com>
2022-05-11 23:04:30 -0300

    Luta com o big boss concluida

--- script.js
+++ script.js
@@ -35,7 +35,8 @@ var cor_porta = "rgb(255, 255, 255)", porta_aberta=false;
 var borda1 = 5, borda2 = canvas.width - 300
 var colisao_oponente, colisao_tiro2
 var cor_oponente2 = 'rgb(100, 100, 100)', oponente2_derrotado = false;
-var x_bigboss = 10, y_bigboss = canvas.height-700, luta_bigboss
+var x_bigboss = 10, y_bigboss = canvas.height-(0.6*canvas.height+200), luta_bigboss
+var big_boss_acertado = 0; big_boss_derrotado = false;
 
 var c = canvas.getContext('2d');
 
@@ -44,11 +45,11 @@ function CriarElementos(){
     c.fillRect(5, canvas.height-111, canvas.width - 50, 30);
 
     c.fillStyle = 'rgb(255, 255, 0)';
-    c.fillRect(5, canvas.height-500, (canvas.width-50), 30);
+    c.fillRect(5, canvas.height-0.6*canvas.height, (canvas.width-50), 30);
 
     //Porta metal
     c.fillStyle = cor_porta;
-    c.fillRect(x_porta, canvas.height-500, canvas.width-1100, 30);
+    c.fillRect(x_porta, canvas.height-0.6*canvas.height, canvas.width-1100, 30);
     
     c.fillStyle = cor_oponente1;
     c.fillRect(x_oponente1, canvas.height-201, 90, 90);
@@ -68,27 +69,27 @@ function FatorSoma(){
 
 function Pular(){
     if(x >= canvas.width - 650 && x <= canvas.width - 360 && plataforma == false){
-        y = canvas.height-600
+        y = canvas.height-(0.6*canvas.height+100)
         plataforma = true;
     }  
     else{
-        y = canvas.height-400
+        y = canvas.height-(0.6*canvas.height-100)
         setTimeout(function(){
             y = canvas.height-211
 
         },500)
     } 
     if(plataforma == true){
-        y = canvas.height - 800
+        y = canvas.height - (0.6*canvas.height+300)
         setTimeout(function(){
-            y = canvas.height - 600
+            y = canvas.height - (0.6*canvas.height+100)
         }, 500)
     }
 }
 
 function CriarNitanerBigBoss(){
     c.fillStyle = cor_oponente2;
-    c.fillRect(x_oponente2, canvas.height-590, 90, 90);
+    c.fillRect(x_oponente2, canvas.height-(0.6*canvas.height+90), 90, 90);
 
     x_oponente2 = x_oponente2 + 5
     
@@ -128,14 +129,38 @@ function AnimateCenario(){
             x_tiro = x
             atirar = false
         }
+        
+    }
+    if(luta_bigboss == true && atirar == true){
+        c.fillStyle = cor_tiro;
+        c.fillRect(x_tiro, y_tiro, 20,20);
+        console.log(x_tiro, x_oponente1)
+    
+        x_tiro=x_tiro-25
+        y_tiro = y_atirar;
+        if(x_tiro == x_bigboss || x_tiro == x_bigboss - 5 || x_tiro == x_bigboss + 5 || x_tiro == x_bigboss - 10 || x_tiro == x_bigboss + 10|| x_tiro == x_bigboss - 15 || x_tiro == x_bigboss + 15 || x_tiro == x_bigboss - 20 || x_tiro == x_bigboss + 20 || x_tiro == x_bigboss - 25 || x_tiro == x_bigboss + 25){
+            big_boss_acertado = big_boss_acertado+1
+            if(big_boss_acertado == 2){
+                big_boss_derrotado = true
+                alert('big boss derrotado')
+            }
+            x_tiro = x
+            atirar = false
+        }
+    }
+    if((x == x_bigboss || x == x_bigboss - 5 || x == x_bigboss + 5 || x == x_bigboss - 10 || x == x_bigboss + 10|| x == x_bigboss - 15 || x == x_bigboss + 15 || x == x_bigboss - 20 || x == x_bigboss + 20 || x == x_bigboss - 25 || x == x_bigboss + 25) && plataforma == true && big_boss_derrotado == false){
+        alert('Perdeu uma vida')
+        location.reload()
     }
     if(colisao_tiro2 == true){
         cor_oponente2 = 'rgb(100, 150, 100)';
         oponente2_derrotado = true;
+        alert('Chegou o momento de enfrentar o Francis, se ele te acertar você será derrotado, são necessários dois tiros de cura para derrotá-lo')
 
     }
     if(x_oponente2 >= canvas.width - 110 || colisao_tiro2 == true){
         luta_bigboss = true
+        colisao_tiro2 = false
     }
     if(luta_bigboss == true){
         setTimeout(function(){
@@ -169,7 +194,7 @@ function AnimateCenario(){
     }
     if(plataforma == true){
         cor_porta = 'rgb(100, 100, 100)'
-        y_atirar = canvas.height - 550
+        y_atirar = canvas.height - (0.6*canvas.height+50)
         CriarNitanerBigBoss()
     }
     else if(plataforma == false){
